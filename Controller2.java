99953e576edc0f885953f5301c46742f47f37de6
aa <12012221@mail.sustech.edu.cn>
2022-11-22 13:11:09 +0800

    second commit

--- Controller2.java
+++ Controller2.java
@@ -1,40 +1,21 @@
 package application.controller;
 
-import application.Client;
-//import application.Listener;
 import application.Server;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
 
-import java.awt.*;
-import java.io.DataInputStream;
-import java.io.IOException;
-import java.io.PrintStream;
-import java.net.Socket;
-import java.net.URL;
-import java.net.UnknownHostException;
-import java.util.ResourceBundle;
-
-public class Controller2 implements Initializable{
+public class Controller2 implements Initializable {
     private static final int PLAY_1 = 0;
     private static final int PLAY_2 = 1;
 
     private static final int EMPTY = -1;
     private static final int BOUND = 90;
     private static final int OFFSET = 15;
-
-    private Client client1;
-
-    private Client client2;
 
     @FXML
     public javafx.scene.control.Button log;
@@ -60,7 +41,9 @@ public class Controller2 implements Initializable{
     @FXML
     public javafx.scene.control.Button bt;
 
-    public Controller2(){};
+    public Controller2() {
+
+    }
 
     private static boolean TURN = false;
 

af96e7ffdf26f99dfb96afd6a0052db8973a8a24
aa <12012221@mail.sustech.edu.cn>
2022-11-22 12:02:34 +0800

    second commit

--- Mserver.java
+++ Mserver.java
@@ -2,7 +2,7 @@ package application;
 
 public class Mserver {
     public static void main(String[] args) {
-        Server server=new Server();
+        Server server = new Server();
         server.init();
     }
 }
